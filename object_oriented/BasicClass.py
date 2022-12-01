cfdbe46300231667ac5fb4aed7f6f2b3224f2a7b
guruvshenoy <guruvshenoy@gmail.com>
2022-12-01 14:41:15 +0530

    basic class outpuy

--- object_oriented/BasicClass.py
+++ object_oriented/BasicClass.py
@@ -32,7 +32,23 @@ print('type(prg1.__dict__)',type(prg1.__dict__))
 
 
 
-
-
-
+######################Output#############
+#Starting the program...
+# Classes in python is of type <class 'type'> class
+# Calling statement - Program.version =  3.6
+# Calling statement - Program.language =  Python
+# Calling statement - getattr(Program,'version') =  3.6
+# Calling statement - getattr(Program,'language') =  Python
+# Printing Program.__dict__ {'__module__': '__main__', 'version': '3.6', 'language': 'Python', '__init__': <function Program.__init__ at 0x7ff90394bd30>, 'greet': <function Program.greet at 0x7ff9039830d0>, '__dict__': <attribute '__dict__' of 'Program' objects>, '__weakref__': <attribute '__weakref__' of 'Program' objects>, '__doc__': None}
+# setting language to 'Java'
+# Calling statement after setting- Program.language =  Java
+# Printing Program.__dict__ {'__module__': '__main__', 'version': '3.6', 'language': 'Java', '__init__': <function Program.__init__ at 0x7ff90394bd30>, 'greet': <function Program.greet at 0x7ff9039830d0>, '__dict__': <attribute '__dict__' of 'Program' objects>, '__weakref__': <attribute '__weakref__' of 'Program' objects>, '__doc__': None, 'os': 'Windows'}
+# Creating object of Program class invokes __init__ of the class
+# Called init...
+# Printing prg1.__dict__ {}
+# Hello,Gurudatt
+# Printing prg1.__dict__ {'name': 'Gurudatt'}
+# type(Program.__dict__) <class 'mappingproxy'>
+# type(prg1.__dict__) <class 'dict'>
+######################Output#############
 
